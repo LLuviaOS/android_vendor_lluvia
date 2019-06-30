AospExtended/platform_vendor_aosp

Add ota version code
* also avoid ota in GSI

Change-Id: Ic82e249065e00942959f17a408ad481d4b0b0b44
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/ota.mk
@@ -12,7 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(IS_GENERIC_SYSTEM_IMAGE), true)
+
+ifeq ($(IS_GO_VERSION), true)
+CUSTOM_OTA_VERSION_CODE := pie_go
+else
+CUSTOM_OTA_VERSION_CODE := pie
+endif
+
+PRODUCT_GENERIC_PROPERTIES += \
+  ro.extended.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
 
 PRODUCT_PACKAGES += \
         Updates
+
+endif
 

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/ota.mk
@@ -12,7 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(IS_GENERIC_SYSTEM_IMAGE), true)
+
+ifeq ($(IS_GO_VERSION), true)
+CUSTOM_OTA_VERSION_CODE := pie_go
+else
+CUSTOM_OTA_VERSION_CODE := pie
+endif
+
+PRODUCT_GENERIC_PROPERTIES += \
+  ro.lluvia.ota.version_code=$(CUSTOM_OTA_VERSION_CODE)
 
 PRODUCT_PACKAGES += \
         Updates
+
+endif
 