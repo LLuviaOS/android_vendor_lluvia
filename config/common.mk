AospExtended/platform_vendor_aosp

Add Pixel's sysconfigs
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/common.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/aosp/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Extra tools
 PRODUCT_PACKAGES += \
     e2fsck \

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/common.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/lib/libsketchology_native.so:system/lib/libsketchology_native.so \
     vendor/lluvia/prebuilt/common/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/lluvia/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Extra tools
 PRODUCT_PACKAGES += \
     e2fsck \