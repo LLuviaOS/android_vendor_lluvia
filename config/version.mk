AospExtended/platform_vendor_aosp

Add ROM's custom fingerprint
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

config/version.mk
@@ -26,8 +26,11 @@ $(call inherit-product-if-exists, vendor/aosp/config/ota.mk)
 
 endif
 
+TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
+
 EXTENDED_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 EXTENDED_MOD_VERSION := AospExtended-$(EXTENDED_VERSION)-$(EXTENDED_BUILD_DATE)-$(EXTENDED_BUILD_TYPE)
+EXTENDED_FINGERPRINT := AospExtended/$(EXTENDED_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(EXTENDED_BUILD_DATE)
 
 
 PRODUCT_GENERIC_PROPERTIES += \
@@ -38,4 +41,5 @@ PRODUCT_GENERIC_PROPERTIES += \
 EXTENDED_DISPLAY_VERSION := AospExtended-$(EXTENDED_VERSION)-$(EXTENDED_BUILD_TYPE)
 
 PRODUCT_GENERIC_PROPERTIES += \
-  ro.extended.display.version=$(EXTENDED_DISPLAY_VERSION)
+  ro.extended.display.version=$(EXTENDED_DISPLAY_VERSION) \
+  ro.extended.fingerprint=$(EXTENDED_FINGERPRINT)

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/version.mk
@@ -26,9 +26,11 @@ $(call inherit-product-if-exists, vendor/lluvia/config/ota.mk)
 
 endif
 
+TARGET_PRODUCT_SHORT := $(subst lluvia_,,$(CUSTOM_BUILD))
+
 LLUVIA_BUILD_DATE := $(shell date -u +%Y%m%d-%H%M)
 LLUVIA_MOD_VERSION := LLuviaOS-$(LLUVIA_VERSION)-$(LLUVIA_BUILD_DATE)-$(LLUVIA_BUILD_TYPE)
-
+LLUVIA_FINGERPRINT := LLuviaOS/$(LLUVIA_VERSION)/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(LLUVIA_BUILD_DATE)
 
 PRODUCT_GENERIC_PROPERTIES += \
   ro.lluvia.version=$(LLUVIA_VERSION) \
@@ -38,4 +40,5 @@ PRODUCT_GENERIC_PROPERTIES += \
 LLUVIA_DISPLAY_VERSION := LLuviaOS-$(LLUVIA_VERSION)-$(LLUVIA_BUILD_TYPE)
 
 PRODUCT_GENERIC_PROPERTIES += \
-  ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION)
+  ro.lluvia.display.version=$(LLUVIA_DISPLAY_VERSION) \
+  ro.lluvia.fingerprint=$(LLUVIA_FINGERPRINT)