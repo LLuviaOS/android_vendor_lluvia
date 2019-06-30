AospExtended/platform_vendor_aosp

init: Add ADB over network property
Change-Id: I43702c501d37a4d8d88f632d262536466616db42

config/common.mk
@@ -214,9 +214,9 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 
-# AEX-specific init file
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.aosp.rc
+# AEX-specific init files
+$(foreach f,$(wildcard vendor/aosp/prebuilt/common/etc/init/*.rc),\
+    $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/common.mk
@@ -44,7 +44,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/etc/permissions/aex-power-whitelist.xml:system/etc/sysconfig/lluvia-power-whitelist.xml
 
-# Custom AEX packages
+# Custom packages
 ifeq ($(TARGET_USE_GCAM),true)
 PRODUCT_PACKAGES += \
     Gcam
@@ -200,9 +200,9 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 endif
 
-# lluvia-specific init file
-PRODUCT_COPY_FILES += \
-    vendor/lluvia/prebuilt/common/etc/init.local.rc:root/init.lluvia.rc
+# LLUVIA-specific init files
+$(foreach f,$(wildcard vendor/lluvia/prebuilt/common/etc/init/*.rc),\
+    $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \