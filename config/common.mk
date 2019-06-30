AospExtended/platform_vendor_aosp

Remove clean cache script
While this method of clearing out the package cache works, it doesn't for
ALL our devices. On devices such as the Pixel and Pixel 2XL, this method
can't and does not work.

Revert "Add clean cache script [1/3]"

This reverts commit f99460ba6650ea388fe96422c82f02dca860fc0f.

Change-Id: Icc205af6f6fe21d0bfe94bc6d8ba24aebdc57956

config/common.mk
@@ -20,8 +20,7 @@ PRODUCT_BRAND ?= AEX
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/aosp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/aosp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
-    vendor/aosp/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+    vendor/aosp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh
 
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/common.mk
@@ -20,8 +20,7 @@ PRODUCT_BRAND ?= LLUVIA
 PRODUCT_COPY_FILES += \
     vendor/lluvia/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/lluvia/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/lluvia/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
-    vendor/lluvia/prebuilt/common/bin/clean_cache.sh:system/bin/clean_cache.sh
+    vendor/lluvia/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh
 
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \