87bae4b36887b9e2e75f002172250626d826b42b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-13 16:22:36 -0300

    vendor: Add support for dark bootanimation
    The dark theme state is stored on misc partition, that follow pixel behavior
    
    As reference:
    
    https://android.googlesource.com/device/google/crosshatch/+/refs/tags/android-10.0.0_r2/init.hardware.rc#994
    
    https://android.googlesource.com/platform/bootable/recovery/+/35e0f6d290ea7c12164bcfaf1b857d965e943938
    
    Change-Id: Ia2d9d163f84638d0c61b3d2e7161301aad7fc774

--- config/common.mk
+++ config/common.mk
@@ -138,6 +138,14 @@ DEVICE_PACKAGE_OVERLAYS += vendor/lluvia/overlay/common
 PRODUCT_PACKAGES += \
     PixelSetupWizardOverlay
 
+# Themed bootanimation
+TARGET_MISC_BLOCK_OFFSET ?= 0
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
+PRODUCT_PACKAGES += \
+    misc_writer_system \
+    themed_bootanimation
+
 # Branding
 include vendor/lluvia/config/version.mk
 
