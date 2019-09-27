32accfdc8fb55b33626f8e18b8a56c96b0de6939
Dyneteve <sound0020@gmail.com>
2019-09-27 15:11:44 +0000

    Revert "vendor: Add special conditions for devices with prebuilt display hal"
    This reverts commit 8a9cf11622e671e4115f831f1e30353164fad23a.
    
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -2,12 +2,6 @@ include vendor/lluvia/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lluvia/config/BoardConfigQcom.mk
-else
-
-ifneq ($(FORCE_QCOM_DISPLAY_HAL_VARIANT),)
-include vendor/lluvia/config/BoardConfigQcomDisplayOverride.mk
-endif
-
 endif
 
 include vendor/lluvia/config/BoardConfigSoong.mk
