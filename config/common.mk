e87133d388da2cc1a2333db04f1582d941bee272
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-10-09 15:30:24 +0200

    Kill sysinit
    Change-Id: Ia8d46d794ff829f3fc39ff48d57fdb6f9c03262d
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -64,16 +64,6 @@ PRODUCT_COPY_FILES += \
     vendor/lluvia/config/permissions/privapp-permissions-lluvia.xml:system/etc/permissions/privapp-permissions-lluvia.xml \
     vendor/lluvia/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml 
 
-# init.d support
-PRODUCT_COPY_FILES += \
-    vendor/lluvia/prebuilt/common/bin/sysinit:system/bin/sysinit
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# userinit support
-PRODUCT_COPY_FILES += \
-    vendor/lluvia/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
-endif
-
 # Copy all custom init rc files
 $(foreach f,$(wildcard vendor/lluvia/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
