TheParasiteProject/vendor_parasite_common

Revert "vendor: Nuke vendorsetup for now"
This reverts commit 33fe11ae01353de596e0b460c25de25485b33bc5.

vendorsetup.sh
@@ -1,0 +1,6 @@
+for device in $(python vendor/aosp/tools/get_official_devices.py)
+do
+for var in user userdebug; do
+add_lunch_combo aosp_$device-$var
+done
+done

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

vendorsetup.sh
@@ -1,0 +1,6 @@
+for device in $(python vendor/lluvia/tools/get_official_devices.py)
+do
+for var in user userdebug; do
+add_lunch_combo lluvia_$device-$var
+done
+done