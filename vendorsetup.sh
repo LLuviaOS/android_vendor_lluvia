TheParasiteProject/vendor_parasite_common

vendor: Add others targets on lunch menu [2/2]
Change-Id: Ic9cfc1c11d011c09718818921475b685340d9a1c

vendorsetup.sh
@@ -1,6 +1,7 @@
+lunch_others_targets=()
 for device in $(python vendor/aosp/tools/get_official_devices.py)
 do
-for var in user userdebug; do
-add_lunch_combo aosp_$device-$var
-done
+    for var in user userdebug; do
+        lunch_others_targets+=("aosp_$device-$var")
+    done
 done

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

vendorsetup.sh
@@ -1,6 +1,7 @@
+lunch_others_targets=()
 for device in $(python vendor/lluvia/tools/get_official_devices.py)
 do
-for var in user userdebug; do
-add_lunch_combo lluvia_$device-$var
-done
+    for var in user userdebug; do
+        lunch_others_targets+=("lluvia_$device-$var")
+    done
 done