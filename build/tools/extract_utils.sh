ff5d9bd68e4c84b1ce0819991f5b4347e592f45b
Han Wang <416810799@qq.com>
2020-03-10 09:40:47 +0200

    extract_utils: Fix up 4a2b65fb60af23b6beccd8c405fc7cea90b9474d
    Change-Id: I9e5ea799812ca932de95ec6cbdedb604cbde5736
    Signed-off-by: Ram9199 <sriramscience7@gmail.com>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -1198,11 +1198,11 @@ function oat2dex() {
     fi
 
     if [ -z "$VDEXEXTRACTOR" ]; then
-        export VDEXEXTRACTOR="$lluvia_ROOT"/prebuilts/tools-lineage/{HOST}-x86/bin/vdexExtractor
+        export VDEXEXTRACTOR="$lluvia_ROOT"/prebuilts/tools-lineage/${HOST}-x86/bin/vdexExtractor
     fi
 
     if [ -z "$CDEXCONVERTER" ]; then
-        export CDEXCONVERTER="$lluvia_ROOT"/prebuilts/tools-lineage/{HOST}-x86/bin/compact_dex_converter
+        export CDEXCONVERTER="$lluvia_ROOT"/prebuilts/tools-lineage/${HOST}-x86/bin/compact_dex_converter
     fi
 
     # Extract existing boot.oats to the temp folder
