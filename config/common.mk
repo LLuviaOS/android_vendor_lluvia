AOSiP/platform_vendor_aosip

vendor: redo and split our priv-app permissions
Change-Id: Ib4c11b9f6665c6983e07a85a860da663edb9a80b
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

config/common.mk
@@ -82,7 +82,8 @@ endif
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/aosip/prebuilt/common/etc/permissions/aosip-privapp-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/aosip-privapp-permissions.xml \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-aosip-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-aosip.xml \
+    vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-aosip.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-aosip.xml \
     vendor/aosip/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.weather.client.xml \
     vendor/aosip/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
 

LLuviaOS/android_vendor_lluvia

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/common.mk
@@ -82,7 +82,8 @@ endif
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/lluvia/prebuilt/common/etc/permissions/lluvia-privapp-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/lluvia-privapp-permissions.xml \
+    vendor/lluvia/prebuilt/common/etc/permissions/privapp-permissions-lluvia-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lluvia.xml \
+    vendor/lluvia/prebuilt/common/etc/permissions/privapp-permissions-lluvia.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lluvia.xml \
     vendor/lluvia/prebuilt/common/etc/permissions/org.pixelexperience.weather.client.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.weather.client.xml \
     vendor/lluvia/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-elgoog.xml
 