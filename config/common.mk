DerpLab/platform_vendor_aosip

vendor: Add a bunch of accents for ThemePicker
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

config/common.mk
@@ -116,6 +116,9 @@ include vendor/aosip/config/fonts.mk
 # Packages
 include vendor/aosip/config/packages.mk
 
+# Themes
+include vendor/aosip/config/accents.mk
+
 # Versioning
 include vendor/aosip/config/version.mk
 

LLuviaOS/android_vendor_lluvia

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Change-Id: Id6b86f7057597874d15c6a18364098fd3557abbc
Signed-off-by: Ram9199 <sriramscience7@gmail.com>

config/common.mk
@@ -95,5 +95,8 @@ include vendor/lluvia/config/packages.mk
 # Prebuilts
 $(call inherit-product-if-exists, vendor/prebuilts/packages.mk)
 
+# Themes
+include vendor/lluvia/config/accents.mk
+
 # Versioning
 include vendor/lluvia/config/version.mk